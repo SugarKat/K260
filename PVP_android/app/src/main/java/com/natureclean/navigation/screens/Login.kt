e7bfea78d57b86f16beba725bb2b0b309930e045
simsni-ktu <simonas.snioka@ktu.edu>
2023-03-14 21:54:22 +0200

    UI related changes

--- PVP_android/app/src/main/java/com/natureclean/navigation/screens/Login.kt
+++ PVP_android/app/src/main/java/com/natureclean/navigation/screens/Login.kt
@@ -13,12 +13,19 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.natureclean.api.model.UserCredentials
 import com.natureclean.navigation.Tabs
+import com.natureclean.viewmodels.MainViewModel
 
 @Composable
-fun Login(navController: NavController) {
+fun Login(navController: NavController, mainViewModel: MainViewModel) {
     var username by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var passwordConfirmation by remember { mutableStateOf("") }
+
+    var error by remember { mainViewModel.errorMessage }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -31,20 +38,53 @@ fun Login(navController: NavController) {
         TextField(
             value = username,
             onValueChange = { username = it },
-            placeholder = {Text("user@email.com")},
-            label = {Text("username")}
+            placeholder = { Text("user") },
+            label = { Text("username") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            placeholder = { Text("user@email.com") },
+            label = { Text("email") }
         )
         Spacer(modifier = Modifier.height(16.dp))
         TextField(
             value = password,
             onValueChange = { password = it },
             visualTransformation = PasswordVisualTransformation(),
-            label = {Text("password")}
+            label = { Text("password") }
 
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { navController.navigate(Tabs.Map.route) }) {
+        TextField(
+            value = passwordConfirmation,
+            onValueChange = { passwordConfirmation = it },
+            visualTransformation = PasswordVisualTransformation(),
+            label = { Text("password") }
+
+        )
+        Button(onClick = {
+            mainViewModel.loginUser(
+                userCreds = UserCredentials(
+                    name = username,
+                    email = email,
+                    password = password,
+                    password_confirmation = passwordConfirmation
+                )
+            ) {
+                navController.navigate(Tabs.Map.route)
+            }
+        }
+        ) {
             Text("Login")
+        }
+
+        error.apply {
+            if (this.isNotEmpty()) {
+                Text(this)
+
+            }
         }
         Spacer(modifier = Modifier.weight(1f))
     }
