9a8336019c743a0c6a155332eac5bfd2e3f63dbc
IGNLEG <legotaignas@gmail.com>
2023-03-14 18:29:37 +0200

    Typo fixes

--- PVP_backend/app/Models/PollutionPoint.php
+++ PVP_backend/app/Models/PollutionPoint.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Pollution_point extends Model
+class PollutionPoint extends Model
 {
     use HasFactory;
 
