5157ecbe3eb1ef42b2b1043ae79b5de448a121e3
simsni-ktu <simonas.snioka@ktu.edu>
2023-04-23 20:16:55 +0300

    tabs and navigation update

--- PVP_android/app/src/main/java/com/natureclean/navigation/Navigation.kt
+++ PVP_android/app/src/main/java/com/natureclean/navigation/Navigation.kt
@@ -10,6 +10,7 @@ import com.example.test_pvp.navigation.tabs.Account
 import com.natureclean.navigation.screens.Login
 import com.natureclean.navigation.screens.Register
 import com.natureclean.navigation.tabs.Containers
+import com.natureclean.navigation.tabs.Leaderboard
 import com.natureclean.navigation.tabs.Map
 import com.natureclean.viewmodels.MainViewModel
 
@@ -23,20 +24,12 @@ fun Navigation(
         navController = navController as NavHostController,
         startDestination = Tabs.Map.route
     ) {
-        // Main tabs
-        composable(Tabs.Account.route) {
-            Account(
-                navController = navController,
-
-                )
-        }
+        composable(Tabs.Account.route) { Account(navController = navController,) }
         composable(Tabs.Map.route) { Map(mainViewModel = mainViewModel) }
         composable(Tabs.Containers.route) { Containers(mainViewModel) }
-        composable(Screen.Login.route) {
-            Login(navController, mainViewModel)
-        }
-        composable(Screen.Register.route) {
-            Register(navController = navController, mainViewModel = mainViewModel)
-        }
+        composable(Tabs.Leaderboard.route) { Leaderboard(mainViewModel = mainViewModel) }
+        composable(Screen.Login.route) { Login(navController, mainViewModel) }
+        composable(Screen.Register.route) { Register(navController = navController, mainViewModel = mainViewModel) }
+
     }
 }
--- PVP_android/app/src/main/java/com/natureclean/navigation/Tabs.kt
+++ PVP_android/app/src/main/java/com/natureclean/navigation/Tabs.kt
@@ -1,12 +1,21 @@
 package com.natureclean.navigation
 
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Leaderboard
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Map
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.ui.graphics.vector.ImageVector
 
-sealed class Tabs(val route: String, val title: String) {
-    object Map: Tabs("map", "Map")
-    object Containers : Tabs("containers", "Containers")
-    object Account : Tabs("account", "Account")
+
+sealed class Tabs(val route: String, val title: String, val icon: ImageVector) {
+    object Map: Tabs("map", "Map", icon = Icons.Filled.Map)
+    object Containers : Tabs("containers", "Containers", icon = Icons.Filled.List)
+    object Account : Tabs("account", "Account", icon = Icons.Filled.Person)
+    object Leaderboard: Tabs("leaderboard", "Leaderboard", icon = Icons.Filled.Leaderboard)
 }
 
 val bottomBarTabs = listOf(
-    Tabs.Map, Tabs.Containers, Tabs.Account,
+    Tabs.Map, Tabs.Containers, Tabs.Leaderboard, Tabs.Account
 )
