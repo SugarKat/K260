2ec5397496ea23b0cd27b2208ff0c61d995a858a
SugarKat <46972873+SugarKat@users.noreply.github.com>
2023-05-24 20:50:24 +0300

    seeder

--- PVP_android/app/src/main/java/com/natureclean/di/AppModule.kt
+++ PVP_android/app/src/main/java/com/natureclean/di/AppModule.kt
@@ -82,7 +82,7 @@ object AppModule {
             //http://10.0.2.2:8000/ //EMULATOR
             //http://192.168.0.101:8000
 
-            .baseUrl("http://192.168.13.95:8080")
+            .baseUrl("http://10.0.2.2:8000/")
             .build()
             .create(BackendInterface::class.java)
     }
